b743314f6dec6bb06de5769da5f73855b0c81e56
Haripriya-VR <hpvr80343@gmail.com>
2024-01-26 11:40:26 +0530

    db connection made to atlas

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+const databaseURL = process.env.DATABASE_URL
 
-mongoose.connect('mongodb://0.0.0.0:27017/E-world',)
+mongoose.connect(databaseURL)
 .then((res)=>{
-    
+    console.log('connected'); 
 }) 
 .catch((err)=>{
     console.log('error occured during establishing the connection',err);
 })
 
 module.exports = mongoose;
+
