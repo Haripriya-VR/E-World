b5c3a6b8df4a54bb746a217e2efc04d2d744d998
Haripriya-VR <hpvr80343@gmail.com>
2024-01-27 16:53:42 +0530

    mongodb local to atlas

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,25 +1,25 @@
-// const mongoose = require("mongoose");
-// require('dotenv').config();
-// const databaseURL = process.env.DATABASE_URL
-// console.log(databaseURL);
-// mongoose.connect(databaseURL) 
-// .then((res)=>{
-//     console.log('connected');
-// }) 
-// .catch((err)=>{
-//     console.log('error occured during establishing the connection',err);
-// })
-
-// module.exports = mongoose;
-
 const mongoose = require("mongoose");
-
-mongoose.connect('mongodb://0.0.0.0:27017/E-world',)
+require('dotenv').config();
+const databaseURL = process.env.DATABASE_URL
+console.log(databaseURL);
+mongoose.connect("mongodb+srv://hpvr80343:1234@e-world.tgp2bd1.mongodb.net/?retryWrites=true&w=majority") 
 .then((res)=>{
-    
+    console.log('connected');
 }) 
 .catch((err)=>{
     console.log('error occured during establishing the connection',err);
 })
 
 module.exports = mongoose;
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect('mongodb://0.0.0.0:27017/E-world',)
+// .then((res)=>{
+    
+// }) 
+// .catch((err)=>{
+//     console.log('error occured during establishing the connection',err);
+// })
+
+// module.exports = mongoose;
